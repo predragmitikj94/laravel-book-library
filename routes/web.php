ae0f2bf64023bf6d4b8dea5e2d46bc7b470aadf5
Predrag Mitic <predragmitikj@gmail.com>
2025-07-07 15:43:38 +0200

    Refactor routes to use Route::resource for BookController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('books', BookController::class);
+// Route::get('books', [BookController::class, 'index'])->name('books.index');
+// Route::get('books/create', [BookController::class, 'create'])->name('books.create');
+// Route::post('books', [BookController::class, 'store'])->name('books.store');
+// Route::get('books/{book}', [BookController::class, 'show'])->name('books.show');
+// Route::get('books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+// Route::put('books/{book}', [BookController::class, 'update'])->name('books.update');
+// Route::delete('books/{book}', [BookController::class, 'destroy'])->name('books.delete');
