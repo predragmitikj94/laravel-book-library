663e55c1023de53a24979917af6b2504cb574911
Predrag Mitic <predragmitikj@gmail.com>
2025-07-09 14:34:56 +0200

    Finalize auth integration with CRUD protection

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('books', BookController::class);
 });
 
 
-Route::resource('books', BookController::class);
-// Route::get('books', [BookController::class, 'index'])->name('books.index');
-// Route::get('books/create', [BookController::class, 'create'])->name('books.create');
-// Route::post('books', [BookController::class, 'store'])->name('books.store');
-// Route::get('books/{book}', [BookController::class, 'show'])->name('books.show');
-// Route::get('books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
-// Route::put('books/{book}', [BookController::class, 'update'])->name('books.update');
-// Route::delete('books/{book}', [BookController::class, 'destroy'])->name('books.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
